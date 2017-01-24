98d01d2a60827991eb25f5d11d36185e96f43211
vagrant <vagrant@homestead>
2017-01-24 22:00:17 +0000

    switching to bulma

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,13 +11,15 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="{{ asset('css/tether.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/tether-theme-arrows-dark.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-reboot.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-grid.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
+    {{--<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/tether.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/tether-theme-arrows-dark.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/bootstrap-reboot.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/bootstrap-grid.css') }}">--}}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.3.1/css/bulma.css" >
+    {{--<link rel="stylesheet" href="{{ asset('css/app.css') }}" >--}}
 
     @yield('stylesheets')
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -40,61 +42,58 @@
     <![endif]-->
 </head>
 <body>
-<div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+<div>
+    <nav class="nav has-shadow">
+        <div class="nav-left">
+            <!-- Collapsed Hamburger-->
+            <span class="nav-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </span>
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <!-- Branding Image -->
+            <a class="nav-item" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-            </div>
+        <div class="nav-center">
+            <a class="nav-item" href="/log">
+                <span class="icon">
+                    <i class="fa fa-book"></i>
+                </span>
+            </a>
+            <a class="nav-item" href="/progress">
+                <span class="icon">
+                    <i class="fa fa-line-chart"></i>
+                </span>
+            </a>
+        </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="/log">New Log</a></li>
-                    <li><a href="/progress">Progress Charts</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li>
-                                    <a href="{{ url('/logout') }}"
-                                       onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                        Logout
-                                    </a>
-
-                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+        <!-- Right Side Of Navbar -->
+        <div class="nav-right nav-menu">
+            <!-- Authentication Links -->
+            @if (Auth::guest())
+                <a class="nav-item" href="{{ url('/login') }}">Login</a>
+                <a class="nav-item" href="{{ url('/register') }}">Register</a>
+            @else
+                <span class="nav-item">
+                    <a href="#profile" class="button">
+                        {{ Auth::user()->name }}
+                    </a>
+                </span>
+                <span class="nav-item">
+                    <a class="button is-primary" href="{{ url('/logout') }}"
+                       onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </span>
+            @endif
         </div>
     </nav>
     @yield('content')
