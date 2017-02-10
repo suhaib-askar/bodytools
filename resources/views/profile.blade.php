d832d45e7c8db007012c9a1593b96852b2a94c95
vagrant <vagrant@homestead>
2017-02-10 02:49:50 +0000

    dob fields not finished yet

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -36,9 +36,24 @@
 							<label for="birthdate" class="label">Birthdate</label>
 
 							<div class="control">
-								<select id="dobmonth" name="dobmonth" class="select"></select>
-								<select id="dobday" name="dobday" class="select"></select>
-								<select id="dobyear" name="dobyear" class="select"></select>
+								<select id="dobmonth" name="dobmonth" class="input select">
+									<option value="0">Month</option>
+									<option value="1">January</option>
+									<option value="2">February</option>
+									<option value="3">March</option>
+									<option value="4">April</option>
+									<option value="5">May</option>
+									<option value="6">June</option>
+									<option value="7">July</option>
+									<option value="8">August</option>
+									<option value="9">September</option>
+									<option value="10">October</option>
+									<option value="11">November</option>
+									<option value="12">December</option>
+								</select>
+								<input type="text" id="dobday" name="dobday" class="input" placeholder="Day" size="5" />
+								<input type="text" id="dobyear" name="dobyear" class="input" placeholder="Year" size="10" />
+
 
 								@if ($errors->has('birthdate'))
 									<span class="help-block">
